1273ffc2cc49f37c2ac4d3313d13e9740facad56
Waqas Umar <55554486+WAQASUMAR33@users.noreply.github.com>
2025-05-19 13:04:49 +0500

    4th  commit

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import { useAuth } from '../lib/auth';
+import { useAuth } from '../lib/authContext';
 import { useRouter } from 'next/navigation';
 
 export default function LoginPage() {
